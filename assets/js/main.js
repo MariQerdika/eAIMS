4218cbb5e59609fe6e34bfb7e39b6ce698bdf98d
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2019-06-10 19:13:25 +0400

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,36 @@ $(document).ready(function () {
         $('.left_arrow').toggleClass('effect_class');
     });
     // end show-hide content
-
-
 });
 
 
+
+// modal
+document.querySelectorAll('.modalBtn').forEach(function (elem) {
+    elem.addEventListener('click', function (e) {
+        this.parentElement.querySelector('.modal').style.display = 'block'
+    });
+});
+document.querySelectorAll('.closeBtn').forEach(function (elem) {
+    elem.addEventListener('click', function (e) {
+        this.parentElement.parentElement.parentElement.style.display = 'none'
+    });
+});
+window.addEventListener('click', outsideClick);
+function outsideClick(e) {
+    if (e.target.classList.length > 0) {
+        if (e.target.classList[0] == 'modal') {
+            e.target.style.display = 'none';
+        }
+    }
+}
+
+
+$("#showmenul").click(function (e) {
+    $("#menul").toggleClass("showl");
+});
+$(document).ready(function () {
+    $("#showmenul").click(function () {
+        $('.left_arrow').toggleClass('effect_class');
+    });
+});
